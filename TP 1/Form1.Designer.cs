fc1d717674c6c53918a033523a9519bdf1e1c705
Fiffah <101395835+Fiffah@users.noreply.github.com>
2022-04-23 13:40:52 +0700

    first commit

---
+++ TP 1/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace TP_1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Textb = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Textb
+            // 
+            this.Textb.Location = new System.Drawing.Point(301, 208);
+            this.Textb.Name = "Textb";
+            this.Textb.Size = new System.Drawing.Size(88, 27);
+            this.Textb.TabIndex = 0;
+            this.Textb.Text = "Submit";
+            this.Textb.UseVisualStyleBackColor = true;
+            this.Textb.Click += new System.EventHandler(this.Tombol_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(265, 163);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(167, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(262, 126);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Masukan Nama Mahasiswa";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Textb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Textb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
